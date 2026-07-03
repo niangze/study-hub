3ed78656cc0de4f97548b78a03c628cccb01b1f7
niangze <3273013679@qq.com>
2026-07-04 02:19:55 +0800

    Create AnswerMapper interface for Answer entity

---
+++ backend/src/main/java/com/studyhub/mapper/ResourceMapper.java
@@ -0,0 +1,9 @@
+package com.studyhub.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.studyhub.entity.Answer;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AnswerMapper extends BaseMapper<Answer> {
+}
