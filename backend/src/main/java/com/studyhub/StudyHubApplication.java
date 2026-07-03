cfe61d02ede1e33a4ab2ee87ec42dd49a324f83c
niangze <3273013679@qq.com>
2026-07-04 02:07:07 +0800

    Update StudyHubApplication.java

--- backend/src/main/java/com/studyhub/StudyHubApplication.java
+++ backend/src/main/java/com/studyhub/StudyHubApplication.java
@@ -1,1 +1,13 @@
+package com.studyhub;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.studyhub.mapper")
+public class StudyHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudyHubApplication.class, args);
+    }
+}
