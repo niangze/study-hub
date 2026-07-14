e6e95e212b77b3fe24a7e8db70890ea4cde5a9ae
niangze <3273013679@qq.com>
2026-07-14 11:58:33 +0800

    fix: 导航栏根据角色条件渲染"后台管理"入口

--- frontend/src/components/AddHeader.vue
+++ frontend/src/components/AddHeader.vue
@@ -9,6 +9,7 @@
       <el-button link @click="$router.push('/questions')">问答</el-button>
       <el-button link @click="$router.push('/resources')">资源</el-button>
       <el-button link @click="$router.push('/profile')">个人中心</el-button>
+      <el-button v-if="userStore.isAdmin" link type="warning" @click="$router.push('/admin')">后台管理</el-button>
       <el-button v-if="userStore.isLoggedIn" type="danger" link @click="handleLogout">退出</el-button>
       <el-button v-else link @click="$router.push('/login')">登录</el-button>
     </div>
