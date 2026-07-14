fb9f5d946af0f9943064d888dfeb82b2085f3fa4
niangze <3273013679@qq.com>
2026-07-14 12:10:32 +0800

    fix: 注册时保存 studentId，getProfile 返回 studentId

--- backend/src/main/java/com/studyhub/service/UserService.java
+++ backend/src/main/java/com/studyhub/service/UserService.java
@@ -32,6 +32,7 @@ public class UserService extends ServiceImpl<UserMapper, User> {
         user.setUsername(request.getUsername());
         user.setPassword(passwordEncoder.encode(request.getPassword()));
         user.setEmail(request.getEmail());
+        user.setStudentId(request.getStudentId());
         user.setAvatar("https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png");
         user.setPoints(100);
         user.setRole("USER");
